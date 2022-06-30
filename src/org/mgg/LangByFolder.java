c70b5d10b5d4fdbf172a140e796e3f82c1ed3bf4
Mauricio Gracia Gutierrez <mauriciogracia@users.noreply.github.com>
2022-06-30 00:53:59 -0500

    Show stackTrace error when iterating

--- src/org/mgg/LangByFolder.java
+++ src/org/mgg/LangByFolder.java
@@ -185,6 +185,8 @@ public class LangByFolder {
         }
         catch (Exception ex) {
             System.err.println("Error:" + ex.getMessage());
+            //@todo: unix links are being shown here, needs to be fixed
+            ex.printStackTrace(System.err);
         }
     }
 
