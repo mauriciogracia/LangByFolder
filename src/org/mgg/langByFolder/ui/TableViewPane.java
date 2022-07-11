0ef73f381c641a9988fe5080f5d69189670a1368
Mauricio Gracia <mauriciogracia@gmail.com>
2022-07-10 20:55:13 -0500

    Dynamic columns by detected language

--- src/org/mgg/langByFolder/ui/TableViewPane.java
+++ src/org/mgg/langByFolder/ui/TableViewPane.java
@@ -4,6 +4,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
+import org.mgg.langByFolder.DirectoryContext;
 import org.mgg.langByFolder.FileContext;
 import org.mgg.langByFolder.ReportOptions;
 
@@ -37,12 +38,38 @@ public class TableViewPane extends BorderPane implements IReportEventsListener{
     @Override
     public void started() {
         tableView.getItems().clear();
+
+        //remove the dynamic language stats columns
+        if(tableView.getColumns().size() > 8)
+        {
+            tableView.getColumns().remove(8, tableView.getColumns().size());
+        }
     }
 
     @Override
     public void finished() {
         for(FileContext fc : rep.items ) {
             tableView.getItems().add(fc);
+        }
+        addLangStatsColumns() ;
+    }
+
+    void addLangStatsColumns() {
+        Object obj = tableView.getItems().get(0) ;
+        String titles = ((DirectoryContext) obj).getLangStats() ;
+
+        String [] titleParts = titles.split(";") ;
+
+        int i = 0 ;
+        int j = i ;
+
+        for(i = 0; i < titleParts.length/2 ; i++ ) {
+            TableColumn<FileContext, String> col =
+                    new TableColumn<>(titleParts[j]);
+
+            //col.setCellValueFactory(new PropertyValueFactory<>(FileContext.getPropertyNameForColumn(i)));
+            tableView.getColumns().add(col);
+            j+= 2 ;
         }
     }
 }
